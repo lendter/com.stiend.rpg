843cff3c8a532967298632cd2157e66cfa6c6fa5
leonendter <LeoEnd@schueler.bwv-ac.de>
2023-11-05 12:09:55 +0100

    use magic talent

--- src/character/Wizard.java
+++ src/character/Wizard.java
@@ -1,24 +1,25 @@
 package character;
 
 import physics.Position;
+import wizarding.MagicTalent;
+import wizarding.PerformStyle;
 
 public class Wizard extends PlayerCharacter{
-
+	private MagicTalent talent;
+	
 	@Override
 	public void move(Position position) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void attack(PlayerCharacter opponent) {
-		// TODO Auto-generated method stub
-		
+		talent.performMagic(this, opponent, PerformStyle.Attack);
 	}
 
 	@Override
 	public boolean defend(PlayerCharacter opponent) {
-		// TODO Auto-generated method stub
+		talent.performMagic(this, opponent, PerformStyle.Defense);
 		return false;
 	}
 	
