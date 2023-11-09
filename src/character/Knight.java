852b4af18ed0b821b2fe4a89552a0817e2d6844d
unknown <endter_leo@K2AC.LOCAL>
2023-11-09 08:44:35 +0100

    add constructor for subclasses

--- src/character/Knight.java
+++ src/character/Knight.java
@@ -1,6 +1,12 @@
 package character;
 
+import items.Weapon;
+
 public class Knight extends Fighter{
+	
+	public Knight(String name, int hp, int intelligence, int strength, int constitution, int dexterity, Weapon weapon) {
+		super(name, hp, intelligence, strength, constitution, dexterity, weapon);
+	}
 	
 	@Override
 	public void attack(PlayerCharacter opponent) {
