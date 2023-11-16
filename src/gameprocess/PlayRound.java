1aeeef738a31d035c840fd701d230eac6649b973
unknown <endter_leo@K2AC.LOCAL>
2023-11-16 08:53:48 +0100

    add Directions; getAvailableMoves

--- src/gameprocess/PlayRound.java
+++ src/gameprocess/PlayRound.java
@@ -1,5 +1,46 @@
 package gameprocess;
 
-public class PlayRound {
+import java.util.ArrayList;
+import java.util.List;
 
+import character.PlayerCharacter;
+import physics.Direction;
+import physics.Map;
+import physics.Position;
+
+public class PlayRound {
+	private Map map;
+	
+	public PlayRound(Map map) {
+		this.setMap(map);
+	}
+
+	public Map getMap() {
+		return map;
+	}
+
+	public void setMap(Map map) {
+		this.map = map;
+	}
+	
+	public List<Position> getAvailableMoves(PlayerCharacter character){
+		List<Position> availablePositions = new ArrayList<>();
+		Position currentPosition = character.getPosition();
+		Position nextWallX = map.getNextWall(currentPosition, Direction.X);
+		Position nextWallY = map.getNextWall(currentPosition, Direction.Y);
+		int xBound = 3;
+		int yBound = 3;
+		if(nextWallX != null) {
+			xBound = nextWallX.getX();
+		}
+		if(nextWallY != null) {
+			yBound = nextWallY.getY();
+		}
+		for(int i = 0; i < xBound; i++) {
+			for(int j = 0; j <= yBound; j++) {
+				availablePositions.add(new Position(i, j));
+			}
+		}
+		return availablePositions;
+	}
 }
--- src/physics/Map.java
+++ src/physics/Map.java
@@ -17,4 +17,31 @@ public class Map {
 		}
 	}
 	
+	public Field getField(Position position) {
+		return fields[position.getX()][position.getY()];
+	}
+
+	public Field getField(int x, int y) {
+		return fields[x][y];
+	}
+	
+	public Position getNextWall(Position position, Direction direction) {
+		switch(direction) {
+		case X:
+			for(int i = 0; i < 3; i++) {
+				if(getField(i, position.getY()).isWall()) {
+					return new Position(i, position.getY());
+				}
+			}
+			break;
+		case Y:
+			for(int i = 0; i < 3; i++) {
+				if(getField(position.getX(), i).isWall()) {
+					return new Position(position.getY(), i);
+				}
+			}
+			break;
+		}
+		return null;
+	}
 }
